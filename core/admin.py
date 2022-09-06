a77692d55cae366ebfe9c3ebe8f91bf7cb2f78c2
gustavo trentini <gustavotrentini22@gmail.com>
2022-09-06 15:11:42 -0300

    Criação dos modelos

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from core.models import Usuario, Seguir
+from core.models import Usuario, Seguir, Noticia, Topico
 
 admin.site.register(Usuario)
 admin.site.register(Seguir)
+admin.site.register(Noticia)
+admin.site.register(Topico)
 # Register your models here.
--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class Usuario(models.Model):
     def __str__(self):
@@ -7,8 +8,7 @@ class Usuario(models.Model):
     email = models.CharField(max_length=120)
     senha = models.CharField(max_length=45)
     tipousuario = models.IntegerField(default=0)
-    img = models.ImageField()
-    banner = models.ImageField()
+
 
 class Seguir(models.Model):
     def __str__(self):
@@ -17,3 +17,23 @@ class Seguir(models.Model):
     usuarioSeguidor = models.ForeignKey(Usuario, on_delete=models.PROTECT, related_name='seguidores')
     class Meta:
         verbose_name_plural = "Seguir"
+
+class Topico(models.Model):
+    def __str__(self):
+        return self.nometopico
+    nometopico = models.CharField(max_length=45)
+
+class Noticia(models.Model):
+    def __str__(self):
+        return self.noticiatitulo
+    noticiatitulo = models.CharField(max_length=180)
+    texto = models.CharField(max_length=7000)
+    noticiadatacadastro = models.DateField(default=datetime.now)
+    user_iduser = models.ForeignKey(Usuario, on_delete=models.PROTECT)
+    topico_idtopico = models.ForeignKey(Topico, on_delete=models.PROTECT)
+
+class Comentario():
+    datacomentario = models.DateField(default=datetime.now)
+    textocomentario = models.CharField(max_length=240)
+    user_iduser = models.ForeignKey(Usuario, on_delete=models.PROTECT)
+    noticia_idnoticia = models.ForeignKey(Noticia, on_delete=models.PROTECT)
