14f0d0b2c4394e82cb2324c7d330abfbe3d0ad50
Gustavo Henrique Trentini <gustavotrentini22@gmail.com>
2022-09-10 14:54:32 -0300

    Usuário e seguidores

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from core.models import Usuario, Seguir, Noticia, Topico
+from core.models import Usuario, Noticia, Topico, Curtida, Comentario
 
 admin.site.register(Usuario)
-admin.site.register(Seguir)
 admin.site.register(Noticia)
 admin.site.register(Topico)
+admin.site.register(Curtida)
+admin.site.register(Comentario)
 # Register your models here.
--- core/models.py
+++ core/models.py
@@ -1,22 +1,18 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 
-class Usuario(models.Model):
-    def __str__(self):
-        return self.nome
-    nome = models.CharField(max_length=120)
-    email = models.CharField(max_length=120)
-    senha = models.CharField(max_length=45)
-    tipousuario = models.IntegerField(default=0)
+class Usuario(AbstractUser):
+    seguidores = models.ManyToManyField('self', symmetrical=False, related_name='seguindo')
 
-
-class Seguir(models.Model):
-    def __str__(self):
-        return (self.usuarioSeguidor.nome + ' seguindo ' + self.usuarioSeguido.nome)
-    usuarioSeguido = models.ForeignKey(Usuario, on_delete=models.PROTECT, related_name='seguindo')
-    usuarioSeguidor = models.ForeignKey(Usuario, on_delete=models.PROTECT, related_name='seguidores')
-    class Meta:
-        verbose_name_plural = "Seguir"
+# class Seguir(models.Model):
+#     def __str__(self):
+#         return (self.usuarioSeguidor.nome + ' seguindo ' + self.usuarioSeguido.nome)
+#     usuarioSeguido = models.ForeignKey(get_user_model(), on_delete=models.PROTECT, related_name='seguindo')
+#     usuarioSeguidor = models.ForeignKey(get_user_model(), on_delete=models.PROTECT, related_name='seguidores')
+#     class Meta:
+#         verbose_name_plural = "Seguir"
 
 class Topico(models.Model):
     def __str__(self):
@@ -29,11 +25,19 @@ class Noticia(models.Model):
     noticiatitulo = models.CharField(max_length=180)
     texto = models.CharField(max_length=7000)
     noticiadatacadastro = models.DateField(default=datetime.now)
-    user_iduser = models.ForeignKey(Usuario, on_delete=models.PROTECT)
+    user_iduser = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
     topico_idtopico = models.ForeignKey(Topico, on_delete=models.PROTECT)
 
-class Comentario():
+class Comentario(models.Model):
+    def __str__(self):
+        return f'({self.noticia_idnoticia}) {self.user_iduser}: {self.textocomentario}'
     datacomentario = models.DateField(default=datetime.now)
     textocomentario = models.CharField(max_length=240)
-    user_iduser = models.ForeignKey(Usuario, on_delete=models.PROTECT)
+    user_iduser = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
     noticia_idnoticia = models.ForeignKey(Noticia, on_delete=models.PROTECT)
+
+class Curtida(models.Model):
+    def __str__(self):
+        return f'{self.iduser} -> {self.idnoticia}'
+    iduser = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
+    idnoticia = models.ForeignKey(Noticia, on_delete=models.PROTECT)
