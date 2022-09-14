96b9180faa768812025f7d09cedba66764d54e87
Gustavo Henrique Trentini <gustavotrentini22@gmail.com>
2022-09-14 19:00:12 -0300

    Relacionamento foreign keys corrigido

--- core/models.py
+++ core/models.py
@@ -25,19 +25,19 @@ class Noticia(models.Model):
     noticiatitulo = models.CharField(max_length=180)
     texto = models.CharField(max_length=7000)
     noticiadatacadastro = models.DateField(default=datetime.now)
-    user_iduser = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
-    topico_idtopico = models.ForeignKey(Topico, on_delete=models.PROTECT)
+    user_iduser = models.ForeignKey(get_user_model(), on_delete=models.PROTECT, related_name="noticias")
+    topico_idtopico = models.ForeignKey(Topico, on_delete=models.PROTECT, related_name="noticias")
 
 class Comentario(models.Model):
     def __str__(self):
         return f'({self.noticia_idnoticia}) {self.user_iduser}: {self.textocomentario}'
     datacomentario = models.DateField(default=datetime.now)
     textocomentario = models.CharField(max_length=240)
-    user_iduser = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
-    noticia_idnoticia = models.ForeignKey(Noticia, on_delete=models.PROTECT)
+    user_iduser = models.ForeignKey(get_user_model(), on_delete=models.PROTECT, related_name="comentarios")
+    noticia_idnoticia = models.ForeignKey(Noticia, on_delete=models.PROTECT, related_name="comentarios")
 
 class Curtida(models.Model):
     def __str__(self):
         return f'{self.iduser} -> {self.idnoticia}'
-    iduser = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
-    idnoticia = models.ForeignKey(Noticia, on_delete=models.PROTECT)
+    iduser = models.ForeignKey(get_user_model(), on_delete=models.PROTECT, related_name="curtidas")
+    idnoticia = models.ForeignKey(Noticia, on_delete=models.PROTECT, related_name="curtidas")
