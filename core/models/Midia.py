8fdd95c150c6b91126fe25c3586838f154bee19e
Gustavo Henrique Trentini <gustavotrentini22@gmail.com>
2022-10-04 10:15:00 -0300

    Midia delete cascade

--- core/models/Midia.py
+++ core/models/Midia.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class Midia(models.Model):
     midiapath = models.CharField(max_length=5000)
-    noticia_idnoticia = models.ForeignKey(Noticia, on_delete=models.PROTECT, related_name="midia")
+    noticia_idnoticia = models.ForeignKey(Noticia, on_delete=models.CASCADE, related_name="midia")
