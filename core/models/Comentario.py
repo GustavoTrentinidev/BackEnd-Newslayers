0e4519cd0c39fddf9330c86a5ad463d0614ff7a9
Gustavo Henrique Trentini <gustavotrentini22@gmail.com>
2022-10-09 20:19:32 -0300

    data do comentario corrigida

--- core/models/Comentario.py
+++ core/models/Comentario.py
@@ -3,11 +3,12 @@ from datetime import datetime
 from .Noticia import Noticia
 from django.contrib.auth import get_user_model
 
+date = str(datetime.now()).split(' ')[0]
 
 class Comentario(models.Model):
     def __str__(self):
         return f'({self.noticia_idnoticia}) {self.user_iduser}: {self.textocomentario}'
-    datacomentario = models.DateField(default=datetime.now)
+    datacomentario = models.DateField(default=date)
     textocomentario = models.CharField(max_length=240)
     user_iduser = models.ForeignKey(get_user_model(), on_delete=models.PROTECT, related_name="comentarios")
     noticia_idnoticia = models.ForeignKey(Noticia, on_delete=models.PROTECT, related_name="comentarios")
