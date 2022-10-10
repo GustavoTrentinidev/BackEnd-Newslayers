7c55ab122e29e3f2525c1e1d8345236dc46ac259
Maria Laura Viana <marialauraviana@hotmail.com>
2022-10-10 11:33:08 -0300

    DjangoModelPermissionsOrAnonReadOnly

--- newslayers/settings.py
+++ newslayers/settings.py
@@ -125,3 +125,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.Usuario'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ]
+}
