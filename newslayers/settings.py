d603a8be3934a3b677b6081259e1ab3a12f26ea6
Gustavo Henrique Trentini <gustavotrentini22@gmail.com>
2022-10-15 18:12:54 -0300

    Remove barra

--- newslayers/settings.py
+++ newslayers/settings.py
@@ -168,4 +168,4 @@ AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 
-CSRF_TRUSTED_ORIGINS = ["https://backend-newslayers-production.up.railway.app/"]
+CSRF_TRUSTED_ORIGINS = ["https://backend-newslayers-production.up.railway.app"]
