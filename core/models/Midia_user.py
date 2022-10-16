d5be58958d7fef574f6b179bb05afc618894d36e
Gustavo Henrique Trentini <gustavotrentini22@gmail.com>
2022-10-16 02:07:53 -0300

    Midia delete cascade

--- core/models/Midia_user.py
+++ core/models/Midia_user.py
@@ -5,4 +5,4 @@ from django.contrib.auth import get_user_model
 class Midia_user(models.Model):
     midiabannerpath = models.FileField(upload_to='media/')
     midiaprofilepath = models.FileField(upload_to='media/')
-    user_iduser = models.OneToOneField(get_user_model(), on_delete=models.PROTECT, related_name="midia")
+    user_iduser = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name="midia")
